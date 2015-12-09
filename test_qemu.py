b9a34591f1adac765be5b7c94983bae545a4dbd1
Scott Bronson <brons_git@rinspin.com>
2015-12-09 15:28:52 -0800

    put forwarding in per-domain chains, start testing

--- test_qemu.py
+++ test_qemu.py
@@ -1,6 +1,8 @@
 import imp
 import json
+import os
 import socket
+import sys
 import unittest
 
 qemu = imp.load_source('qemu', 'qemu')
@@ -12,7 +14,7 @@ class QemuTestCase(unittest.TestCase):
         try:
             socket.inet_aton(host_ip)
         except socket.error:
-            pass  # Not legal 
+            pass  # Not legal
 
     def test_config(self):
         conf = qemu.config(validate=False)
@@ -21,3 +23,22 @@ class QemuTestCase(unittest.TestCase):
     def test_config_schema_validation(self):
         conf = qemu.config()
         del qemu.config._conf  # Revert closure
+
+    def test_setup(self):
+        port_map = {
+            "tcp": [[80, 8080], 443]
+        }
+
+        qemu.IPTABLES_BINARY = '/bin/echo'
+        outfile = "/tmp/ttp"
+        orig = os.dup(sys.stdout.fileno())
+        try:
+            to = open(outfile, "w")
+            os.dup2(to.fileno(), sys.stdout.fileno())
+            qemu.start_forwarding(port_map, "DNAT-test", "FWD-test", "192.168.1.1", "127.0.0.1")
+        finally:
+            sys.stdout.flush()
+            os.dup2(orig, sys.stdout.fileno())
+
+    def test_teardown(self):
+        pass
